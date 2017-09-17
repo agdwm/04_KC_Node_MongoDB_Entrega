3d9fb0df9341bba9a21166a287c95d76df2adcc8
agdwm <>
2017-09-17 20:25:58 +0200

    Imported the library express-validator

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const {query, validationResult } = require('express-validator/check');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
