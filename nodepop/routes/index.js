d2ee31384b0affad69d6d3252e4f2d5c3ab4f03c
agdwm <>
2017-09-24 21:21:59 +0200

    Created the structure of the public folder and configured gulpfile

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -5,7 +5,7 @@ const {query, validationResult } = require('express-validator/check');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Nodepop' });
 });
 
 //Ejemplo Validacion
