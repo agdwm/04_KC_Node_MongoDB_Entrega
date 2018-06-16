2d00ca22278cc3994f686bafc420d1eef94bcdb0
agdwm <almudenaguerraswm@gmail.com>
2018-06-16 22:45:59 +0200

    Validated the query param 'price' in the url

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -12,11 +12,40 @@ const Advertisement = mongoose.model('Advertisement');
 /* GET home page. */
 router.get('/:lang(es|en)?', [
 	// Query String Validations wity express-validator
-	query('isSale').isBoolean().optional().withMessage('must be one of these values: true or false')
+	query('isSale').isBoolean().optional().withMessage('debe ser uno de los siguientes valores: true o false'),
+	query('price').custom((value) => {
+		const price = parseFloat(value);
+
+		if (value.indexOf('-') >= 0) {
+			const range = value.split('-');
+			const pmin = parseFloat(range[0]);
+			const pmax = parseFloat(range[1]);
+
+			if (range.length > 2) {
+				return false;
+			}
+			if (pmin !== '' && pmax !== '') {
+				if (!Number.isNaN(pmin) && !Number.isNaN(pmax)) {
+					if (pmin >= pmax) {
+						return false;
+					}
+				}
+			}
+			if (pmin !== '' && !Number.isNaN(pmin)) {
+				return true;
+			}
+			if (pmax !== '' && !Number.isNaN(pmax)) {
+				return true;
+			}
+			return false;
+		} else if (price !== '' && !Number.isNaN(price)) {
+			return true;
+		}
+		return false;
+	}).withMessage('debe ser un valor numérico o un rango válido'),
 ], (req, res, next) => {
 	validationResult(req).throw();
 
-	// const xhr = ;
 	const filter = Advertisement.addFilter(req);
 	// console.log('FILTER', filter);
 
@@ -26,7 +55,7 @@ router.get('/:lang(es|en)?', [
 			next(err);
 			return;
 		}
-		// console.log("SERVIDOR", advertisements);
+		// If it is an Ajax request
 		if (req.xhr) {
 			res.render('partials/adsList', {
 				advertisements: advertisements
