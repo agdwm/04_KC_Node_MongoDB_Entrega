d3f8b8592689d2d6dea36a491a2c0f298c66af99
agdwm <almudenaguerraswm@gmail.com>
2018-06-29 16:29:36 +0200

    Refactored the paginateService in order to adapt it to the new function that create the data object

--- nodepop/src/js/services/paginateService.js
+++ nodepop/src/js/services/paginateService.js
@@ -9,8 +9,18 @@ export default class PaginateService {
 		this.skip = 0;
 		this.limit = parseInt($.trim($('#pagination-limit').attr('data-limit')), 10);
 
-		this.totalAds = parseInt($.trim($('#pagination-total').attr('data-total')), 10); // total de anuncios q recibimo en cada petición
 		this.dataSource = [];
+
+		// this.paginationTotalKey = $.trim($('#pagination-total').attr('data-type'));
+
+	}
+
+	setTotalAds() {
+		const currentTotalAds = $.trim($('#pagination-total').attr('data-total'));
+		$('#pagination-total').attr('data-total', currentTotalAds);
+		this.totalAds = parseInt(currentTotalAds, 10);
+		console.log('currentTotalAds', currentTotalAds);
+		console.log('TotalAds', this.totalAds);
 	}
 
 	// Array with the Total of elements
@@ -24,10 +34,16 @@ export default class PaginateService {
 	}
 
 	// Start value of every array of ads
-	generateCurrentSkip() {
-		const currentSkip = ((this.currentBtn * this.limit) - this.limit);
+	generateCurrentSkip(currentBtn) {
+		const currentSkip = ((currentBtn * this.limit) - this.limit);
 		this.skip = currentSkip;
+		console.log('currentSkip1', this.skip);
 	}
+
+	// generateTotalVal() {
+	// 	this.skip = this.skip.toString();
+	// 	this.limit = this.limit.toString();
+	// }
 
 	initPaginate(self, pagContainer) {
 		pagContainer.pagination({
@@ -37,15 +53,46 @@ export default class PaginateService {
 			showGoButton: true,
 			showBeginingOnOmit: false,
 			showEndingOnOmit: false,
+			hideWhenLessThanOnePage: true,
 			pageRange: 1,
 			prevText: '<i class="glyphicon glyphicon-chevron-left"></i>',
 			nextText: '<i class="glyphicon glyphicon-chevron-right"></i>',
 			callback: (data, pagination) => {
-				this.currentBtn = pagination.pageNumber;
-				this.generateCurrentSkip();
-				self.generateData(this.skip, this.limit);
+				console.log('Load DataSource', this.dataSource);
+				const currentBtn = pagination.pageNumber;
+				this.generateCurrentSkip(currentBtn);
+				self.dataService.createData({ skip: this.skip, limit: this.limit });
 				self.loadAds(self.dataService.getData());
 			}
 		});
+	}
+
+	// En este caso necesito pasarle el currentBtn porque la paginacion se 
+	// ejecuta despues del generateCurrentSkip. el generateCurrentSkip necesita 
+	// ejecutarse antes para renderizar los anuncios
+	renderPaginate(self, pagContainer) {
+		pagContainer.pagination({
+			dataSource: this.dataSource, // total ads from filtered request
+			pageSize: this.limit, // num max ads per page
+			showGoInput: true,
+			showGoButton: true,
+			showBeginingOnOmit: false,
+			showEndingOnOmit: false,
+			pageRange: 1,
+			hideWhenLessThanOnePage: true,
+			prevText: '<i class="glyphicon glyphicon-chevron-left"></i>',
+			nextText: '<i class="glyphicon glyphicon-chevron-right"></i>',
+			callback: (data, pagination) => {
+				console.log('Ajax DataSource', this.dataSource);
+				const currentBtn = pagination.pageNumber;
+				this.generateCurrentSkip(currentBtn);
+				console.log('this.skip2', this.skip);
+				this.generateTotalVal();
+				self.generateData(this.paginationTotalKey, this.paginationTotalVal);
+				self.loadAdsPag(self.dataService.getData());
+				// self.generateData(this.skip, this.limit);
+				// self.loadAds(self.dataService.getData());
+			}
+		});		
 	}
 }
